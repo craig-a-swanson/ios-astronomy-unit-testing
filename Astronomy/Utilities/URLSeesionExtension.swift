769ce2bcbea5c20ad66db543ab49740dbf37cc8d
Craig Swanson <cswanson02@me.com>
2020-02-12 00:43:32 -0600

    Implementing protocol.  Completed network version of loadData(), but still need to complete local version.

---
+++ Astronomy/Utilities/URLSeesionExtension.swift
@@ -0,0 +1,33 @@
+//
+//  URLSeesionExtension.swift
+//  Astronomy
+//
+//  Created by Craig Swanson on 2/12/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession: NetworkDataLoader {
+    func loadData(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        let loadDataTask = dataTask(with: request) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error fetching data: \(error)")
+            }
+            
+            guard let data = data else {
+                completion(nil, error)
+                return
+            }
+            completion(data, nil)
+        }
+        loadDataTask.resume()
+    }
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        completion(
+    }
+    
+    
+}
